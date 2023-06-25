6033cff1e23c8a9b1d0b825d72c1e66f02c1b05f
Jack Dewinter <jackdewinter1@gmail.com>
2023-06-25 15:57:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This project uses [poetry](https://python-poetry.org/docs/) for dependency
+management.
+
+Install dependencies:
+
+```
+poetry install
+```
+
+Copy .env.sample into a new .env file and add your credentials.
---
+++ main.py
@@ -0,0 +1,25 @@
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, PromptTemplate
+from langchain.chat_models import PromptLayerChatOpenAI
+from dotenv import load_dotenv
+
+def create_prompt():
+  human_message_prompt = HumanMessagePromptTemplate(
+      prompt=PromptTemplate(
+        template="My name is {name}",
+        input_variables=["name"],
+      )
+    )
+  return ChatPromptTemplate.from_messages([human_message_prompt])
+
+
+def main():
+  chat_prompt = create_prompt()
+  messages = chat_prompt.format_prompt(name="YOUR_NAME").to_messages()
+
+  model = PromptLayerChatOpenAI(model_name="gpt-3.5-turbo", pl_tags=["langchain"])
+  output = model(messages)
+  print(output)
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
