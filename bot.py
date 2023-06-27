bd61926e0d926a2e83b09e9e01743f148ce9edc6
Jack Dewinter <jackdewinter1@gmail.com>
2023-06-27 11:50:31 +0100

    have bot ask for input initially

--- bot.py
+++ bot.py
@@ -1,17 +1,14 @@
-import os
 from langchain import PromptTemplate, LLMChain
 from langchain.llms import PromptLayerOpenAI
 import chainlit as cl
 from dotenv import load_dotenv
 load_dotenv()
 
-template = """Question: {question}
-
-Answer: Let's think step by step."""
-
-@cl.langchain_factory(use_async=True)
-def factory():
-    prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=PromptLayerOpenAI(temperature=0, pl_tags=["docs-discussion"]), verbose=True)
-
-    return llm_chain
+@cl.on_chat_start
+async def main():
+    res = await cl.AskUserMessage(content="Hi, I'm here to help you learn. Enter the URL for the documentation or describe what you need and I'll find some relavent docs.",
+                                   timeout=10).send()
+    if res:
+        await cl.Message(
+            content=f"Looking into {res['content']} now...",
+        ).send()
